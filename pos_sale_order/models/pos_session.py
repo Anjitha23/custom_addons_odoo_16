5e40854fc7577d66c8fbfc240e6e69618d634690
Anjitha <anjithavinod23@gmail.com>
2023-12-05 17:18:46 +0530

    sale order smart button

--- pos_sale_order/models/pos_session.py
+++ pos_sale_order/models/pos_session.py
@@ -1,29 +1,39 @@
 # -*- coding: utf-8 -*-
-"""Inheriting pos_session model"""
 from odoo import api, fields, models
-
 
 class PosSession(models.Model):
     _inherit = 'pos.session'
 
-    # Add a One2many field to store related sale orders
-    sale_order_ids = fields.One2many('sale.order',
-                                     'pos_session_id',
-                                     string='Related Sale Orders')
-    sale_order_count = fields.Integer('Sale Order Count',
-                                      compute='_compute_sale_order_count',
-                                      store=True)
+    sale_order_ids = fields.One2many(
+        'sale.order',
+        'pos_session_id',
+        string='Related Sale Orders',
+        domain="[('pos_session_id', '=', id)]"
+    )
+
+    sale_order_count = fields.Integer(
+        'Sale Order Count',
+        compute='_compute_sale_order_count',
+        store=True
+    )
+    print("hii")
 
     @api.depends('sale_order_ids')
     def _compute_sale_order_count(self):
+        print('helo')
         for session in self:
-            session.sale_order_count = len(session.sale_order_ids)
+            print(session)
+            session.sale_order_count = len(session.sale_order_ids.filtered(lambda so: so.pos_session_id == session))
+            print(session.sale_order_count)
 
     def action_open_sale_orders(self):
+        # Get the IDs of sale orders related to the current POS session
+        sale_order_ids = self.sale_order_ids.ids
+
         return {
             'type': 'ir.actions.act_window',
             'name': 'Sale Orders',
-            'view_mode': 'form',
+            'view_mode': 'tree,form',
             'res_model': 'sale.order',
-            'res_id': self.sale_order_ids.id
+            'domain': [('id', 'in', sale_order_ids)],
         }
